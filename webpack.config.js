1212f58de669ebd854146aa5670740b79e487b38
HoracioGutierrez <horacio.estevez@gmail.com>
2020-05-09 21:26:40 -0300

    Add basic html-plugin config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 require("dotenv").config()
 
 module.exports = {
@@ -8,5 +9,15 @@ module.exports = {
     output : {
         path : path.resolve(__dirname,"dist"),
         filename : "bundle.js"
-    }
+    },
+    plugins : [
+        new HtmlWebpackPlugin({
+            template : path.resolve(__dirname,"public","index.html"),
+            minify : {
+                removeAttributeQuotes : true,
+                removeComments : true,
+                collapseWhitespace : true
+            }
+        })
+    ]
 }
