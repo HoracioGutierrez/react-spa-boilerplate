a77dee8b067505cab04541dc48b3b7554f3d2f8f
HoracioGutierrez <horacio.estevez@gmail.com>
2020-05-09 21:21:59 -0300

    Add dotenv

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
 const path = require("path")
+require("dotenv").config()
+
+console.log(process.env.mode)
 
 module.exports = {
     target : "web",
+    mode : process.env.mode || "development",
     entry : path.resolve(__dirname,"src","index.js"),
     output : {
         path : path.resolve(__dirname,"dist"),
