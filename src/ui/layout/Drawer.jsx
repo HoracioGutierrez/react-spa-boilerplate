40ca26721eeac3936d04e93ef5a0399e9762653d
HoracioGutierrez <horacio.estevez@gmail.com>
2020-07-21 14:32:12 -0300

    Add new styling with overlay to drawer

--- src/ui/layout/Drawer.jsx
+++ src/ui/layout/Drawer.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import Nav from './Nav'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { toggleDrawer } from "../../api/actions"
 
-const Drawer = ({drawer_open}) => 
+const Drawer = ({drawer_open,toggleDrawer}) => 
     <div id="layout-drawer" className={drawer_open ? "open" : "closed"}>
-        <button className="material-icons">close</button>
+        <button className="material-icons" onClick={toggleDrawer}>close</button>
         <Nav id="drawer-nav"/>
+        <div id="overlay" className={drawer_open ? "open" : "closed"}></div>
     </div>
 
 
 export default connect(
-    ({drawer_open}) => ({drawer_open})
+    ({drawer_open}) => ({drawer_open}),
+    dispatch => ({
+        toggleDrawer : bindActionCreators(toggleDrawer,dispatch)
+    })
 )(Drawer)
